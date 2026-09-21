79fe02810c9f1b286f0261688f12a8267a174542
Naman Raichura <naman.raichura@gmail.com>
2026-09-21 12:22:57 -0700

    First 30 min.
    Set the foundation of my code

---
+++ Main.java
@@ -0,0 +1,15 @@
+// Volleyball serving simulator
+public class Main{
+    
+    public static void main(String[] args){
+        //
+        System.out.println("======================================================");
+        System.out.println("Welcome to Volleyball serving simulator!==============");
+        System.out.println("======================================================");
+
+
+
+    }
+
+
+}
---
+++ Player.java
@@ -0,0 +1,16 @@
+public class Player {
+    private String name;
+    private int servingSkill;
+    private int successfulServes;
+    private int failedServes;
+    private int totalAttempts;
+    private double accuracy;
+    private boolean finished;
+    public Player(){
+        name="Naman";
+        servingSkill=0;
+    }
+    public static void main(String[] args){
+
+    }
+}
