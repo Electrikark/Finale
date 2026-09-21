6181277bd2585581f8f85fdece996a15b8a13aec
Naman Raichura <naman.raichura@gmail.com>
2026-09-21 13:25:02 -0700

    2nd commit

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
 // Volleyball serving simulator
+import java.util.Scanner;
 public class Main{
-    
     public static void main(String[] args){
         //
-        System.out.println("======================================================");
-        System.out.println("Welcome to Volleyball serving simulator!==============");
-        System.out.println("======================================================");
-
-
+        boolean quit = false;
+        Scanner input = new Scanner(System.in);
+        int option;
+        while (!quit){
+            System.out.println("======================================================");
+            System.out.println("Welcome to Volleyball serving simulator!==============");
+            System.out.println("======================================================\n");
+            System.out.println("1. Start practice \n2. View stats \n3. Personal Best \n4. Quit ");
+            option = input.nextInt();
+            
+            break;
+        }
 
     }
 
--- Player.java
+++ Player.java
@@ -1,16 +1,86 @@
 public class Player {
-    private String name;
-    private int servingSkill;
+    public String name;
+    public String club;
+    private int servingSkill; // 1 = float - 2 = top spin - 3 = short - 
     private int successfulServes;
     private int failedServes;
     private int totalAttempts;
-    private double accuracy;
-    private boolean finished;
+    public double accuracy;
+    public boolean finished = false;
+    static int numPlayers;
     public Player(){
+        numPlayers++;
         name="Naman";
-        servingSkill=0;
+        club = "Bay-to-Bay";
+        servingSkill=1;
+        
     }
-    public static void main(String[] args){
+    public Player(String n, int ss, String c){
+        numPlayers++;
+        name = n;
+        club = c;
+        servingSkill= ss;
+    }
+    public String Serve(){
+        double value;
+        double calibrator;
+        totalAttempts++;
+        if (this.servingSkill==1){ // float serve
+            value = ((Math.random()*5)+3.0); // 3.0 - 9.0
+            calibrator = (value/10.0)*100;
+            if (calibrator>=50){
+                successfulServes++;
+                return "Successful Serve!";
+            }
+            else{
+                failedServes++;
+                return "Failed Serve!";
+            }
 
+        }
+        if (this.servingSkill==2){ // top spin
+            value = ((Math.random()*3)+3.0); // 3-6 
+            calibrator = (value/10.0)*100;
+            if (calibrator>=50){
+                successfulServes++;
+                return "Successful Serve!";
+            }
+            else{
+                failedServes++;
+                return "Failed Serve!";
+            }
+        }
+        if (this.servingSkill==3){ // short serve
+            value= (Math.random()*3)+4.0;
+            calibrator = (value/10.0)*100;
+            if (calibrator>=50.0){
+                successfulServes++;
+                return "Successful Serve!";
+            }
+            else{
+                failedServes++;
+                return "Failed Serve!";
+            }
+
+        }
+        else{
+            return "";
+        }
+
+    }
+    public void viewStats(){
+        for (int i =0; i<22; i++){
+            System.out.print("-");
+        }
+        System.out.print("\n Statistics for "+ this.club + "'s player " + this.name + )
+        for (int i =0; i<22; i++){
+            System.out.print("-");
+        }
+    }
+
+
+    public static void main(String[] args){
+        Player player1 = new Player();
+        
     }
 }
